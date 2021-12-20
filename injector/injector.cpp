a2afa0e4f2cd1d17c9467c2cb7ada5f6815dc94d
toms300 <88624703+toms300@users.noreply.github.com>
2021-12-20 17:33:39 +0000

    Update injector.cpp

--- injector/injector.cpp
+++ injector/injector.cpp
@@ -2,7 +2,7 @@
 #include <Windows.h>
 #include <TlHelp32.h>
 
-DWORD GetProdId(const char* procnName)
+DWORD GetProcId(const char* procName)
 {
     DWORD procId = 0;
     HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -10,21 +10,55 @@ DWORD GetProdId(const char* procnName)
     if (hSnap != INVALID_HANDLE_VALUE)
     {
         PROCESSENTRY32 procEntry;
-        procEntry.dwsize = sizeof(procEntry);
+        procEntry.dwSize = sizeof(procEntry);
 
         if (Process32First(hSnap, &procEntry))
         {
             do
             {
-                
-            }
+                if (!_stricmp(procEntry.szExeFile, procName))
+                {
+                    procId = procEntry.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnap, &procEntry));
         }
     }
-
+    CloseHandle(hSnap);
+    return procId;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    const char* dllPath = "C:\\Users\\me\\dll.dll";
+    const char* procName = "ac_client.exe";
+    DWORD procId = 0;
 
+    while (!procId)
+    {
+        procId = GetProcId(procName);
+        Sleep(30);
+    }
+
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+
+    if (hProc && hProc != INVALID_HANDLE_VALUE)
+    {
+        void* loc = VirtualAllocEx(hProc, 0, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+        WriteProcessMemory(hProc, loc, dllPath, strlen(dllPath) + 1, 0);
+
+        HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0);
+
+        if (hThread)
+        {
+            CloseHandle(hThread);
+        }
+    }
+
+    if (hProc)
+    {
+        CloseHandle(hProc);
+    }
+    return 0;
+}
